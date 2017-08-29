e75ae2919e973ad91167d116e9d7697dccc4bf7d
MARTINA_WEIDENBAUM <martinaweidenbaum1@MARTINA-WEIDENBAUMs-MacBook-Pro.local>
2017-08-28 20:04:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 const app = "I don't do much."
+
+function bumpCounter(){
+
+	let counter = 0
+
+	function addBump(){
+		counter += 1
+	}
+	function getBumps(){
+		return counter
+	}
+	return {
+		addBump, 
+		getBumps
+	}
+}
+
+function createAnimal(animalType){
+	return function(deadlyDevice){
+		return{animalType, deadlyDevice}
+	}
+
+}
+
+let sharkCreator = createAnimal('Shark')
+let sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+let sharkWithFrickinCannon = sharkCreator('Cannon')
+
+
+
+// function fatBastard(meal) {
+//   function whatsForDinner() { // whatsForDinner() is the inner function, a closure
+//     if (meal === 'Mini-Me') { // whatsForDinner() uses argument provided to the parent function 
+//       console.log('The wee man is in my belly!');
+//     } else {
+//       console.log(`I'm eatin' a bit of ${meal}! Burp.`);
+//     }
+//   }
+ 
+//   return whatsForDinner;
+// }
